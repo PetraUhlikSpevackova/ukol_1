87340de8c616097bebaaf9b1abec021d99f9a708
PetraUhlikSpevackova <spevackova.petra@seznam.cz>
2024-04-30 19:06:47 +0200

    ukol_1_new

--- ukol_1.py
+++ ukol_1.py
@@ -14,8 +14,8 @@ class Pizza (Item):
         return f"{self.name} pizza za {self.price} Kč, ingredience jsou {self.ingredients}"
     
     def add_extra(self, ingredient, quantity, price_per_ingredient):
-        final_price = self.price + price_per_ingredient
-        return super().__str__() + f", celková cena včetně extra ingrediencí je {final_price} Kč"
+        self.ingredients[ingredient] = quantity*price_per_ingredient
+        self.price += quantity*price_per_ingredient
 
 class Drink (Item):
     def __init__(self, name, volume, price):
@@ -25,18 +25,15 @@ class Drink (Item):
         return f"{self.name}, {self.volume} ml za {self.price} Kč"
 
 class Order:
-    def __init__(self, customer_name, delivery_address, items, status="Delivered"):
+    def __init__(self, customer_name, delivery_address, items, status="New"):
         self.customer_name = customer_name
         self.delivery_address = delivery_address
         self.items = items
         self.status = status
     def mark_delivered(self):
-        if self.status == "Delivered":
-            return "Objednávka doručena"
-        else:
-            return "Objednávka nedoručena"
+        self.status = "Delivered"
     def __str__(self):
-        return f"{self.customer_name}, {self.delivery_address}, {self.items}"
+        return f"{self.customer_name}, {self.delivery_address}, {self.items}, {self.status}"
 
 class DeliveryPerson:
     def __init__(self, name, phone_number, current_order, available = True):
@@ -44,11 +41,16 @@ class DeliveryPerson:
         self.phone_number = phone_number
         self.current_order = current_order
         self.available = available
-    def assign_order(self):
+    def assign_order(self, order):
         if self.available == True:
-            return self.current_order == "Na cestě"
+            self.current_order = order
+            self.available = False
+            print("Přiřazeno")
+        else:
+            print("Nepřiřazeno")
     def complete_delivery(self):
-        if self.current_order == "Doručeno":
-            return self.available == True
+        self.current_order.mark_delivered()
+        self.available = True
+        print("Objednávka doručena")
     def __str__(self):
         return f"{self.name}, {self.phone_number}, {self.available}"
